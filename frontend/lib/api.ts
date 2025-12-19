82af5aad60f2820989897d6a07dc5f5922da9f1c
H0ussamCl4p <choubikhoussam@gmail.com>
2025-12-20 00:00:33 +0100

    latest fix

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -5,5 +5,16 @@ const api = axios.create({
   withCredentials: true, // send cookies if you rely on session cookies
 });
 
+// Add Authorization header interceptor
+api.interceptors.request.use((config) => {
+  const token = typeof document !== 'undefined' ? 
+    (document.cookie.match(/auth_token=([^;]+)/)?.[1] || localStorage.getItem('auth_token')) : null;
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
 export { api };
